34f294d9338711f5183c178f3bce1bf951409133
Dzmitry Maridau <vinmaster@tut.by>
2020-01-03 17:34:56 +0300

    homepage splitted into smaller components, final styling of menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,14 +5,15 @@ import "./menu-item.styles.scss";
 const MenuItem = ({ title, imageUrl, size }) => {
   const SHOP_BUTTON = "SHOP NOW";
   return (
-    <div
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">{SHOP_BUTTON}</span>
       </div>
     </div>
