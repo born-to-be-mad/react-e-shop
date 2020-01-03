298dde0904632d09bf3247470a5a21872723436b
Dzmitry Maridau <vinmaster@tut.by>
2020-01-03 15:39:37 +0300

    Homepage component and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./components/homepage/homepage.component";
+
 function App() {
-  return <div>Hello, world!</div>;
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
 }
 
 export default App;
