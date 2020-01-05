4bb7c45438e4dd15bef6f10111d25ee206f0a304
Dzmitry Maridau <vinmaster@tut.by>
2020-01-05 16:46:16 +0300

    Implement Sign-in and sing-out. Improve styling.

--- src/App.js
+++ src/App.js
@@ -8,17 +8,44 @@ import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up-page.compo
 import HomePage from "./pages/home/home-page.component";
 import ShopPage from "./pages/shop/shop-page.component";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/sign-in" component={SignInSignUpPage} />
-      </Switch>
-    </div>
-  );
+import { auth } from "./firebase/firebase.utils";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    //sunscribe and save the subscrioption to the variable
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user.displayName + "(" + user.email + ") is logged-in");
+      console.dir(user);
+    });
+  }
+
+  componentWillUnmount() {
+    //close subscription
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/sign-in" component={SignInSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
