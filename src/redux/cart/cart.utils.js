d66b5d0c012fe37405811b51bee4986b6f7db40b
Dzmitry Maridau <vinmaster@tut.by>
2020-01-08 00:50:35 +0300

    update shop-data with unique id, new utilily function to group items in cart reducer.

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,12 @@
+export const addItemToCart = (items, itemToAdd) => {
+  const existingItem = items.find(cartItem => cartItem.id === itemToAdd.id);
+  if (existingItem) {
+    return items.map(cartItem =>
+      cartItem.id === itemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...items, { ...itemToAdd, quantity: 0 }];
+};
