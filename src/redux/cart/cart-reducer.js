d6e837596e48d9ccec7db8ca13da04b4efccf42a
Dzmitry Maridau <vinmaster@tut.by>
2020-01-08 00:34:06 +0300

    Add items to state by pressing add-button

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,7 +1,8 @@
 import { CartActionTypes } from "./cart.types";
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +11,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state, //everything else on the state
         hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state, //everything else on the state
+        cartItems: [...state.cartItems, action.payload] // spread existing cart items and add action.payload to the end of this array
       };
     default:
       return state;
