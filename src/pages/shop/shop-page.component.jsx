01299f80dbc35c5f618c8d26eaf30194dc6b2e67
Dzmitry Maridau <vinmaster@tut.by>
2020-01-21 01:14:09 +0300

    Add redux-saga library. Create and use shop sagas.

--- src/pages/shop/shop-page.component.jsx
+++ src/pages/shop/shop-page.component.jsx
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionPageContainer from "../collection/collection-page.container";
 
-import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
 class ShopPage extends React.Component {
   componentDidMount() {
@@ -32,7 +32,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchCollections: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollections: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import createSagaMiddlware from "redux-saga";
 
 import rootReducer from "./root.reducer";
+import rootSaga from "./root-saga";
 
-const middlewares = [thunk];
+const sagaMiddlware = createSagaMiddlware();
+
+const middlewares = [sagaMiddlware];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddlware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
