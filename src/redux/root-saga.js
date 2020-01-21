c1f8ffdc8e5c508281c7a3ad6e67eecfb7ca67a0
Dzmitry Marudau <vinmaster@tut.by>
2020-01-21 19:14:28 +0300

    Comment for root-saga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -3,5 +3,6 @@ import { all, call } from "redux-saga/effects";
 import { onFetchCollectionsStart } from "./shop/shop.sagas";
 
 export default function* rootSaga() {
+  //all expects arrrays of sagas and we can pass any number of sgas to call
   yield all([call(onFetchCollectionsStart)]);
 }
