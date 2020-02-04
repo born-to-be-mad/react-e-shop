6d9b507223e27011b97562650812564f8fa46bd1
Dzmitry Marudau <vinmaster@tut.by>
2020-02-04 11:51:59 +0300

    Use react Profiler component for accessign profile data

--- client/src/pages/home/home-page.component.jsx
+++ client/src/pages/home/home-page.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Profiler } from "react";
 
 import { HomePageContainer } from "./home-page.styles";
 
@@ -7,7 +7,18 @@ import Directory from "../../components/directory/directory.component";
 const HomePage = () => {
   return (
     <HomePageContainer>
-      <Directory />
+      <Profiler
+        id="Directory"
+        onRender={(id, phase, actualDuration) => {
+          console.log({
+            id,
+            phase, //mount or rerender
+            actualDuration //time in ms
+          });
+        }}
+      >
+        <Directory />
+      </Profiler>
     </HomePageContainer>
   );
 };
