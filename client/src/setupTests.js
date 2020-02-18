f27373fccb12090c233cc943e0cdd6094bf98f19
Dzmitry Maridau <vinmaster@tut.by>
2020-02-19 01:21:30 +0300

    Correct configuration of testing libraries for client.

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -4,6 +4,8 @@
 // learn more: https://github.com/testing-library/jest-dom
 //import '@testing-library/jest-dom/extend-expect';
 
+import "jest-enzyme";
+
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
